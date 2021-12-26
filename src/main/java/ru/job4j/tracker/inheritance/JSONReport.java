2d4dc6eef35d096a5938a9ec2d78101a01fed293
markmw <dant1kkz@gmail.com>
2021-12-26 12:04:18 +0600

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/tracker/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + String.format("\t\"name\" : \"%s\",", name) + System.lineSeparator()
