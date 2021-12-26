24bc73e35b87c2b5cf48edbf0b1befeddac8169a
markmw <dant1kkz@gmail.com>
2021-12-26 14:15:04 +0600

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/tracker/encapsulation/pojo/College.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.encapsulation.pojo;
+
+import java.sql.SQLOutput;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Addy");
+        student.setSurname("Baitorynov");
+        student.setGroup("2A");
+        student.setCreated(new Date());
+        System.out.println(student.getName() + " "
+                + student.getSurname() + " from "
+                + student.getGroup() + " at "
+                + student.getCreated());
+    }
+}
---
+++ src/main/java/ru/job4j/tracker/encapsulation/pojo/Student.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker.encapsulation.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String name;
+    private String surname;
+    private String group;
+    private Date created;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
