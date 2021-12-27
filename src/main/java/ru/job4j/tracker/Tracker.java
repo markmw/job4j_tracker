6b8dc50fb96974f7525bda598391809390df5abb
markmw <dant1kkz@gmail.com>
2021-12-27 19:49:46 +0600

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -52,4 +52,16 @@ public class Tracker {
         }
         return index != -1;
     }
+
+    public boolean delete(int id) {
+        boolean rsl = true;
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+            rsl = true;
+        }
+        return rsl;
+    }
 }
