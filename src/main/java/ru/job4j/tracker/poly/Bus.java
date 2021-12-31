6151a409b107ace5daef3232d66b9c215c78344e
markmw <dant1kkz@gmail.com>
2021-12-31 22:39:57 +0600

    3. Интерфейс Input [#181092 #245477]

--- src/main/java/ru/job4j/tracker/poly/Bus.java
+++ src/main/java/ru/job4j/tracker/poly/Bus.java
@@ -4,10 +4,11 @@ public class Bus implements Transport {
     private int count;
     private int fuel;
     private int price;
+    private boolean driver = true;
 
     @Override
     public void drive() {
-
+        this.driver = false;
     }
 
     @Override
