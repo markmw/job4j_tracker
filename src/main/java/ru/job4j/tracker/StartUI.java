f4e109780ce1012feadb93217749659a53cceb13
markmw <dant1kkz@gmail.com>
2021-12-31 22:54:34 +0600

    2.8. Реализация класса StartUI. Вывод заявок по имени [#500749]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -60,6 +60,18 @@ public class StartUI {
                 } else {
                     System.out.println("Not found: " + mark);
                 }
+            } else if (select == 5) {
+                    System.out.println("Find items by name...");
+                    System.out.print("Enter name: ");
+                    String moniker = scanner.nextLine();
+                    Item[] queu = tracker.findByName(moniker);
+                    if (queu.length > 0) {
+                        for (Item item : queu) {
+                        System.out.println(queu);
+                        }
+                    } else {
+                        System.out.println("Not found: " + moniker);
+                    }
             } else if (select == 6) {
                 run = false;
                 System.out.println("Exit Program");
