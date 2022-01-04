e96331f6fe389de8863d6c43f83ef1bec2f8b988
markmw <dant1kkz@gmail.com>
2022-01-04 16:42:24 +0600

    1. Singleton [#94606 #246796]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,6 +2,17 @@ package ru.job4j.tracker;
 
 public class SingleTracker {
     private Tracker tracker = new Tracker();
+    private static SingleTracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
