aaf201289d53242cf86a0ba92da81fd1b562ffca
markmw <dant1kkz@gmail.com>
2022-06-14 10:51:49 +0600

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -34,20 +34,31 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        try (SqlTracker sqlTracker = new SqlTracker()) {
-            sqlTracker.init();
-            List<UserAction> actions = List.of(
-                    new CreateAction(output),
-                    new EditAction(output),
-                    new DeleteAction(output),
-                    new ShowAction(output),
-                    new FindIdAction(output),
-                    new FindNameAction(output),
-                    new ExitAction(output)
-            );
-            new StartUI(output).init(input, sqlTracker, actions);
-        } catch (Exception e) {
-            e.printStackTrace();
+        MemTracker tracker = new MemTracker();
+        List<UserAction> actions = List.of(
+                new CreateAction(output),
+                new EditAction(output),
+                new DeleteAction(output),
+                new ShowAction(output),
+                new FindIdAction(output),
+                new FindNameAction(output),
+                new ExitAction(output)
+        );
+        new StartUI(output).init(input, tracker, actions);
+
+        MemTracker tracker1 = new MemTracker();
+        for (int i = 0; i < 100000; i++) {
+            tracker1.add(new Item("asd" + i));
+            tracker1.add(new Item("asdd" + i));
+            tracker1.add(new Item("asddd" + i));
+            tracker1.add(new Item("asdddd" + i));
+            tracker1.add(new Item("asddddd" + i));
+            tracker1.add(new Item("asdddddd" + i));
+            tracker1.add(new Item("asddddddd" + i));
+            tracker1.replace(i, new Item("asdasd "));
+            tracker1.delete(i + 1);
+            tracker1.findById(i + 3);
+            tracker1.findAll();
         }
     }
 }
